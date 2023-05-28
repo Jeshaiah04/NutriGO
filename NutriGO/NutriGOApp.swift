73a139d41f7a0b5524d31902491fe22bf68ce0d9
Jeshaiah04 <jeshaiahjesse01@student.ciputra.ac.id>
2023-05-28 15:31:03 +0700

    Initial Commit

---
+++ NutriGO/NutriGOApp.swift
@@ -0,0 +1,17 @@
+//
+//  NutriGOApp.swift
+//  NutriGO
+//
+//  Created by MacBook Pro on 28/05/23.
+//
+
+import SwiftUI
+
+@main
+struct NutriGOApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
